604db548c7485e39bc8b2275642594d2b676de4a
akhamesy <akhamesy@sfu.ca>
2021-02-08 16:37:03 -0700

    created table component with minimal styles

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,10 +1,33 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Table from './components/Table'
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/"></Route>
+        <Route path="/">
+          <Table
+            sortable
+            columnWidths={['1fr', '4fr', '2fr']}
+            data={[
+              {
+                name: 'test1',
+                email: 'test2',
+                role: 'owner3',
+              },
+              {
+                name: 'test2',
+                email: 'test3',
+                role: 'owner1',
+              },
+              {
+                name: 'test3',
+                email: 'test1',
+                role: 'owner2',
+              },
+            ]}
+          />
+        </Route>
       </Switch>
     </Router>
   )
---
+++ src/client/src/components/Table.tsx
@@ -0,0 +1,79 @@
+import { useState } from 'react'
+import styles from '../css/Table.module.css'
+import classNames from '../utils/classNames'
+
+export interface ITableProps {
+  classes?: { table?: string; header?: string; data?: string }
+  columnWidths?: string[]
+  data: Record<string, any>[]
+  headers?: string[]
+  excludeHeaders?: boolean
+  sortable?: boolean
+}
+
+const Table = ({
+  sortable,
+  classes,
+  data,
+  headers,
+  columnWidths,
+  excludeHeaders,
+}: ITableProps) => {
+  const dataHeaders = excludeHeaders ? [] : headers ?? Object.keys(data[0])
+  const [sortConfig, setSortConfig] = useState({ by: '', asc: true })
+
+  const sortDataBy = (column: string) => {
+    const asc = sortConfig.by !== column || !sortConfig.asc
+    data.sort((a, b) => {
+      if (a[column] < b[column]) {
+        return asc ? -1 : 1
+      } else if (a[column] > b[column]) {
+        return asc ? 1 : -1
+      }
+      return 0
+    })
+    setSortConfig({
+      by: column,
+      asc,
+    })
+  }
+
+  const numColumns = dataHeaders.length
+  const gridTemplateColumns = columnWidths
+    ? columnWidths.join(' ')
+    : `repeat(1fr, ${numColumns})`
+
+  return (
+    <div
+      style={{ gridTemplateColumns }}
+      className={classNames(styles.table, classes?.header)}
+    >
+      {dataHeaders.map(header => (
+        <div
+          key={header}
+          className={classNames(styles.header, classes?.header)}
+        >
+          {sortable ? (
+            <button
+              className={styles.sortBtn}
+              onClick={() => sortDataBy(header)}
+            >
+              {header}
+            </button>
+          ) : (
+            { header }
+          )}
+        </div>
+      ))}
+      {data.map(row =>
+        Object.entries(row).map(([heading, cell]) => (
+          <div key={heading} className={classNames(styles.data, classes?.data)}>
+            {cell}
+          </div>
+        ))
+      )}
+    </div>
+  )
+}
+
+export default Table
