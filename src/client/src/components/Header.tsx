bfecf0f880122b382c2c4ba254bfc47744c7a00a
gracelu0 <grace.r.luo@gmail.com>
2021-02-09 00:53:01 -0800

    Create header component without toggle functionality

--- src/client/src/components/Header.tsx
+++ src/client/src/components/Header.tsx
@@ -1,8 +1,12 @@
 import styles from '../css/Header.module.css'
 import planet from '../assets/planet-icon.svg'
+import menuIcon from '../assets/icons8-menu-50.png'
 
 const Header: React.FC = () => (
   <div className={styles.div}>
+    <button className={styles.hamburgerMenu}>
+      <img src={menuIcon} />
+    </button>
     <img src={planet} />
     <h1>GitLab Analyzer</h1>
   </div>
