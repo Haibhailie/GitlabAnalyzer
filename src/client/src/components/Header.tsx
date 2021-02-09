49b4f169abf652cb4a33a6c8889b438b6935df79
gracelu0 <grace.r.luo@gmail.com>
2021-02-08 23:55:36 -0800

    Add font and colour variables

--- src/client/src/components/Header.tsx
+++ src/client/src/components/Header.tsx
@@ -1,7 +1,9 @@
 import styles from '../css/Header.module.css'
+import planet from '../assets/planet-icon.svg'
 
 const Header: React.FC = () => (
   <div className={styles.div}>
+    <img src={planet} />
     <h1>GitLab Analyzer</h1>
   </div>
 )
