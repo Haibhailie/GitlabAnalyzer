9b5c830ff87b2949a70d2f5271151302bc113f05
LIYANG ZHOU <liyang_zhou_2@sfu.ca>
2021-02-11 00:15:09 -0800

    set members

--- src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/member/MemberDTO.java
+++ src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/member/MemberDTO.java
@@ -2,5 +2,14 @@ package ca.sfu.orcus.gitlabanalyzer.member;
 
 import org.gitlab4j.api.models.Member;
 
+import java.util.List;
+
 public class MemberDTO {
+    private List<Member> members;
+
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
+    }
+
 }
--- src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/member/MemberRepository.java
+++ src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/member/MemberRepository.java
@@ -1,4 +1,8 @@
 package ca.sfu.orcus.gitlabanalyzer.member;
+import org.gitlab4j.api.GitLabApi;
+import org.gitlab4j.api.GitLabApiException;
+import org.gitlab4j.api.models.*;
+
 
 public class MemberRepository {
 }
