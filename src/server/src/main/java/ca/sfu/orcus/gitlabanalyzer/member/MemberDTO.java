8df95f442af6f81dd57c640c73a11b2733eca9e9
LIYANG ZHOU <liyang_zhou_2@sfu.ca>
2021-02-12 00:02:12 -0800

    get some attributes of a group memeber of a project

--- src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/member/MemberDTO.java
+++ src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/member/MemberDTO.java
@@ -1,6 +1,35 @@
 package ca.sfu.orcus.gitlabanalyzer.member;
 
-
+//[
+//  {
+//    "id": 1,
+//    "username": "raymond_smith",
+//    "name": "Raymond Smith",
+//    "state": "active",
+//    "avatar_url": "https://www.gravatar.com/avatar/c2525a7f58ae3776070e44c106c48e15?s=80&d=identicon",
+//    "web_url": "http://192.168.1.8:3000/root",
+//    "last_activity_on": "2021-01-27"
+//  },
+//  {
+//    "id": 2,
+//    "username": "john_doe",
+//    "name": "John Doe",
+//    "state": "active",
+//    "avatar_url": "https://www.gravatar.com/avatar/c2525a7f58ae3776070e44c106c48e15?s=80&d=identicon",
+//    "web_url": "http://192.168.1.8:3000/root",
+//    "email": "john@example.com",
+//    "last_activity_on": "2021-01-25"
+//  },
+//  {
+//    "id": 3,
+//    "username": "foo_bar",
+//    "name": "Foo bar",
+//    "state": "active",
+//    "avatar_url": "https://www.gravatar.com/avatar/c2525a7f58ae3776070e44c106c48e15?s=80&d=identicon",
+//    "web_url": "http://192.168.1.8:3000/root",
+//    "last_activity_on": "2021-01-20"
+//  }
+//]
 
 public class MemberDTO {
 
--- src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/member/MemberRepository.java
+++ src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/member/MemberRepository.java
@@ -3,7 +3,36 @@ import org.gitlab4j.api.GitLabApi;
 import org.gitlab4j.api.GitLabApiException;
 import org.gitlab4j.api.models.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MemberRepository {
+    public ArrayList<MemberDTO> getAllMembers(GitLabApi gitLabApi, int projectID) throws GitLabApiException {
+
+        ArrayList<MemberDTO> listMember = new ArrayList<>();
+        List<Member> members = gitLabApi.getProjectApi().getAllMembers(projectID);
+        String presentProjectName = gitLabApi.getProjectApi().getProject(projectID).getName();
+
+        System.out.println("\n\nThe present members in " + presentProjectName + " are:");
+
+        for (Member m : members) {
+
+            MemberDTO presentMember = new MemberDTO();
+
+            presentMember.setName(m.getName());
+
+            presentMember.setEmail(m.getEmail());
+
+            presentMember.setId(m.getId());
+
+            presentMember.setState(m.getState());
+
+            presentMember.setUsername(m.getUsername());
+
+            listMember.add(presentMember);
+        }
+        return listMember;
+    }
 
 }
