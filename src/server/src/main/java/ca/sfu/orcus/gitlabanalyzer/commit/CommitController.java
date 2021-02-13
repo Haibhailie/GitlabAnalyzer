e974928e67777617d0e4e5d0c54692c0ac87226c
Taha Liaqat <tliaqat@sfu.ca>
2021-02-12 22:28:07 -0800

    Add endpoints for CommitController. Add method to CommitRepository to get diff of single commit.

--- src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/commit/CommitController.java
+++ src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/commit/CommitController.java
@@ -1,4 +1,36 @@
 package ca.sfu.orcus.gitlabanalyzer.commit;
 
+
+import org.gitlab4j.api.GitLabApi;
+import org.gitlab4j.api.GitLabApiException;
+import org.gitlab4j.api.models.Diff;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class CommitController {
+    private final GitLabApi gitLabApi = null;
+
+    @GetMapping("/api/core/{projectId}/commits")
+    public List<CommitDTO> getCommits(@PathVariable(value="projectId") int projectId) throws GitLabApiException {
+        return CommitRepository.getAllCommits(gitLabApi, projectId);
+    }
+
+    @GetMapping("/api/core/{projectId}/commit/{sha}")
+    public CommitDTO getSingleCommit(@PathVariable(value="projectId") int projectId, @PathVariable(value="sha") String sha) throws GitLabApiException {
+        return CommitRepository.getSingleCommit(gitLabApi, projectId, sha);
+    }
+
+    @GetMapping("/api/core/{projectId}/commit/{sha}/diff")
+    public List<Diff> getSingleCommitDiffs(@PathVariable(value="projectId") int projectId, @PathVariable(value="sha") String sha) throws GitLabApiException {
+        return CommitRepository.getDiffOfCommit(gitLabApi, projectId, sha);
+    }
+
+
+
 }
--- src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/commit/CommitRepository.java
+++ src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/commit/CommitRepository.java
@@ -3,6 +3,7 @@ package ca.sfu.orcus.gitlabanalyzer.commit;
 import org.gitlab4j.api.GitLabApi;
 import org.gitlab4j.api.GitLabApiException;
 import org.gitlab4j.api.models.Commit;
+import org.gitlab4j.api.models.Diff;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -12,7 +13,7 @@ public class CommitRepository {
     private final String defaultBranch = "master";
 
     // Overloaded method - no date required
-    public ArrayList<CommitDTO> getAllCommits(GitLabApi gitLabApi, int projectID) throws GitLabApiException {
+    public static ArrayList<CommitDTO> getAllCommits(GitLabApi gitLabApi, int projectID) throws GitLabApiException {
         ArrayList<CommitDTO> allCommits = new ArrayList<>();
         List<Commit> allGitCommits = gitLabApi.getCommitsApi().getCommits(projectID);
 
@@ -27,7 +28,7 @@ public class CommitRepository {
         return getAllCommitDTOS(gitLabApi, projectID, allCommits, allGitCommits);
     }
 
-    private ArrayList<CommitDTO> getAllCommitDTOS(GitLabApi gitLabApi, int projectID, ArrayList<CommitDTO> allCommits, List<Commit> allGitCommits) throws GitLabApiException {
+    private static ArrayList<CommitDTO> getAllCommitDTOS(GitLabApi gitLabApi, int projectID, ArrayList<CommitDTO> allCommits, List<Commit> allGitCommits) throws GitLabApiException {
         for(Commit c : allGitCommits) {
             CommitDTO presentCommit = new CommitDTO(gitLabApi, projectID, c);
             allCommits.add(presentCommit);
@@ -35,9 +36,12 @@ public class CommitRepository {
         return allCommits;
     }
 
-    private CommitDTO getSingleCommitDTO(GitLabApi gitLabApi, int projectID, String sha) throws GitLabApiException {
+    public static CommitDTO getSingleCommit(GitLabApi gitLabApi, int projectID, String sha) throws GitLabApiException {
         Commit gitCommit = gitLabApi.getCommitsApi().getCommit(projectID, sha);
-        CommitDTO commitDTO = new CommitDTO(gitLabApi, projectID, gitCommit);
-        return commitDTO;
+        return new CommitDTO(gitLabApi, projectID, gitCommit);
+    }
+
+    public static List<Diff> getDiffOfCommit(GitLabApi gitLabApi, int projectID, String sha) throws GitLabApiException {
+        return gitLabApi.getCommitsApi().getDiff(projectID, sha);
     }
 }
