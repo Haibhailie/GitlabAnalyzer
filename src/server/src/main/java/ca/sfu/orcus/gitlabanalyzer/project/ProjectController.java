ecd19bcc3cb5c3cd440ee5b2126e152a66d464a1
Karan Pathania <kpathani@sfu.ca>
2021-02-14 18:30:39 -0800

    - Update endpoints

--- src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/project/ProjectController.java
+++ src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/project/ProjectController.java
@@ -1,6 +1,7 @@
 package ca.sfu.orcus.gitlabanalyzer.project;
 
 import org.gitlab4j.api.GitLabApi;
+import org.gitlab4j.api.GitLabApiException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,24 +11,25 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/projects")
 public class ProjectController {
     private final ProjectService projectService;
-    // TODO: Need to get the GitLabApi after authentication is completed
-    private final GitLabApi gitLabApi = null;
+    // TODO: Need to provide GitLabApi after authentication is set up
+    private final GitLabApi gitLabApi = GitLabApi.oauth2Login("http://cmpt373-1211-09.cmpt.sfu.ca",
+            "user",
+            "pass");
 
     @Autowired
-    public ProjectController(ProjectService projectService) {
+    public ProjectController(ProjectService projectService) throws GitLabApiException {
         this.projectService = projectService;
     }
 
-    @GetMapping
-    public List<ProjectDto> getAllProjects() {
+    @GetMapping(path = "/api/projects")
+    public List<ProjectDto> getAllProjects() throws GitLabApiException {
         return projectService.getAllProjects(gitLabApi);
     }
 
-    @GetMapping(path = "{projectId")
-    public ProjectDto getProject(@PathVariable("projectId") int projectId) {
+    @GetMapping(path = "/api/core/{projectId}/project")
+    public ProjectDto getProject(@PathVariable("projectId") int projectId) throws GitLabApiException {
         return projectService.getProject(gitLabApi, projectId);
     }
 }
