5bfcc7d4380df3a41778590f15e9ec15fdc2f5bd
gracelu0 <grace.r.luo@gmail.com>
2021-02-16 03:01:27 -0800

    Remove css properties with no effect

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/"></Route>
+        <Route path="/">
+          <Header />
+        </Route>
         <Route path="/home"></Route>
       </Switch>
     </Router>
