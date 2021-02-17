58c72d605fbe9053f2c6bcba2745a20ef9982fc8
djso <djso@sfu.ca>
2021-02-16 16:56:42 -0800

    Add login by user and pass logic to AuthenticationRepository

--- src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/authentication/AuthenticationRepository.java
+++ src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/authentication/AuthenticationRepository.java
@@ -19,10 +19,21 @@ public class AuthenticationRepository {
         collection.insertOne(generateUserPatDoc(newUser));
     }
 
+    public void addNewUserByUserPass(User newUser) {
+        collection.insertOne(generateUserAuthTokenDoc(newUser));
+    }
+
     private Document generateUserPatDoc(User newUser) {
         return new Document("_id", new ObjectId())
                 .append("type", "test_pat")
                 .append("pat", newUser.getPat())
+                .append("jwt", newUser.getJwt());
+    }
+
+    private Document generateUserAuthTokenDoc(User newUser) {
+        return new Document("_id", new ObjectId())
+                .append("type", "authToken")
+                .append("authToken", newUser.getAuthToken())
                 .append("jwt", newUser.getJwt());
     }
 }
