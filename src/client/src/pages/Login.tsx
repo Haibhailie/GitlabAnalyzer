7d2a969122ff049ee806fbf90e0a657b90c51d8d
gracelu0 <grace.r.luo@gmail.com>
2021-02-17 02:37:38 -0800

    Refactor css to use flexbox and changes suggested in code review

--- src/client/src/pages/Login.tsx
+++ src/client/src/pages/Login.tsx
@@ -1,15 +1,15 @@
 import styles from '../css/Login.module.css'
 import astronaut from '../assets/splash.svg'
 
-const Login: React.FC = () => {
+const Login = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.splash}>
-        <img src={astronaut} alt="Astronaut holding flag" />
-        <div className={styles.loginButtonDiv}>
-          <button className={styles.loginButton}>Login</button>
-        </div>
-      </div>
+    <div className={styles.splash}>
+      <img
+        className={styles.splashImg}
+        src={astronaut}
+        alt="Astronaut holding flag"
+      />
+      <button className={styles.loginButton}>Login</button>
     </div>
   )
 }
