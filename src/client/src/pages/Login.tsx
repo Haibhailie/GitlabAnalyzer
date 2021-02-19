8a4e59d741790b57dfb64167b0eb123088689d18
djso <djso@sfu.ca>
2021-02-18 19:41:54 -0800

    Add login form fields to login page

--- src/client/src/pages/Login.tsx
+++ src/client/src/pages/Login.tsx
@@ -1,15 +1,22 @@
 import styles from '../css/Login.module.css'
 import astronaut from '../assets/splash.svg'
+import LoginFields from '../components/LoginFields'
 
 const Login = () => {
   return (
     <div className={styles.splash}>
-      <img
-        className={styles.splashImg}
-        src={astronaut}
-        alt="Astronaut holding flag"
-      />
-      <button className={styles.loginButton}>Login</button>
+      <div className={styles.logoContainer}>
+        <img
+          className={styles.splashImg}
+          src={astronaut}
+          alt="Astronaut holding flag"
+        />
+      </div>
+
+      {/* <button className={styles.loginButton}>Login</button> */}
+      <div className={styles.loginContainer}>
+        <LoginFields />
+      </div>
     </div>
   )
 }
