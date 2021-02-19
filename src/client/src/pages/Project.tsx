f59fb224f4e2dfd3841f4b41844cc7b34f0b4008
akhamesy <akhamesy@sfu.ca>
2021-02-18 19:48:54 -0700

    template project and css

---
+++ src/client/src/pages/Project.tsx
@@ -0,0 +1,11 @@
+import styles from '../css/Project.module.css'
+
+const Project = () => {
+  return (
+    <div className={styles.container}>
+      <h1></h1>
+    </div>
+  )
+}
+
+export default Project
