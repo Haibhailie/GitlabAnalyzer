953147429f87da7b62c80e5dd06331ddc725e57b
Karan Pathania <kpathani@sfu.ca>
2021-02-19 21:47:53 -0800

    Update ProjectDto

--- src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/project/ProjectDto.java
+++ src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/project/ProjectDto.java
@@ -1,33 +1,20 @@
 package ca.sfu.orcus.gitlabanalyzer.project;
 
-import org.gitlab4j.api.GitLabApi;
-import org.gitlab4j.api.GitLabApiException;
-import org.gitlab4j.api.models.Owner;
 import org.gitlab4j.api.models.Project;
-
-import java.util.Date;
 
 public class ProjectDto {
     private Integer id;
     private String name;
-    private String description;
-    private Boolean isPublic;
-    private Owner owner;
-    private Date lastActivityAt;
-    private Date createdAt;
-    private String defaultBranch;
-    private long storageSize;
+    private String role;
+    private Long lastActivityAt;
+    private Boolean analyzed;
 
-    public ProjectDto(GitLabApi gitLabApi, Project project) throws GitLabApiException {
+    public ProjectDto(Project project) {
         setId(project.getId());
         setName(project.getName());
-        setDescription(project.getDescription());
-        setPublic(project.getPublic());
-        setOwner(project.getOwner());
-        setLastActivityAt(project.getLastActivityAt());
-        setCreatedAt(project.getCreatedAt());
-        setDefaultBranch(project.getDefaultBranch());
-        setStorageSize(project.getStatistics().getStorageSize());
+        setRole("TODO");
+        setLastActivityAt(project.getLastActivityAt().getTime());
+        setAnalyzed(false); // TODO
     }
 
     public void setId(Integer id) {
@@ -38,31 +25,15 @@ public class ProjectDto {
         this.name = name;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setRole(String role) {
+        this.role = role;
     }
 
-    public void setPublic(Boolean isPublic) {
-        this.isPublic = isPublic;
-    }
-
-    public void setOwner(Owner owner) {
-        this.owner = owner;
-    }
-
-    public void setLastActivityAt(Date lastActivityAt) {
+    public void setLastActivityAt(Long lastActivityAt) {
         this.lastActivityAt = lastActivityAt;
     }
 
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public void setDefaultBranch(String defaultBranch) {
-        this.defaultBranch = defaultBranch;
-    }
-
-    public void setStorageSize(long storageSize) {
-        this.storageSize = storageSize;
+    public void setAnalyzed(Boolean analyzed) {
+        this.analyzed = analyzed;
     }
 }
