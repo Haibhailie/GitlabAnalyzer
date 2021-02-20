e7acd139b542b5b57156082d70cc754a83687fa8
Shashank Thanalapati <shashank_thanalapati@sfu.ca>
2021-02-19 16:36:36 -0800

    Extracted functions for Date in MergeRequestController

--- src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/mergeRequest/MergeRequestController.java
+++ src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/mergeRequest/MergeRequestController.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class MergeRequestController {
 
     private final MergeRequestService mergeRequestService;
+    int epochMillisecondScale = 1000; //to multiply the long from parseLong() by 1000 to convert to milliseconds, for Java's date constructor
 
     @Autowired
     public MergeRequestController(MergeRequestService mergeRequestService) {
@@ -28,24 +29,26 @@ public class MergeRequestController {
                                                   @RequestParam (required = false) String since,
                                                   @RequestParam (required = false) String until) throws GitLabApiException{
 
-        //to multiply the long from parseLong() by 1000 to convert to milliseconds, for Java's date constructor
-        int epochMillisecondScale = 1000;
-        Date dateSince, dateUntil;
+        Date dateSince = calculateSince(since);
+        Date dateUntil = calculateUntil(until);
         Gson gson = new Gson();
-        if(since == null)
-            dateSince = new Date(0); //since the beginning of time
-        else
-            dateSince = new Date(Long.parseLong(since) * epochMillisecondScale); // since given value
-
-        if(until == null)
-            dateUntil = new Date(); // until now
-        else
-            dateUntil = new Date(Long.parseLong(until) * epochMillisecondScale); // until given value
-
         List <MergeRequestDTO> mergeRequestDTOS = mergeRequestService.getAllMergeRequests(jwt, projectId, dateSince, dateUntil);
         response.setStatus(mergeRequestDTOS == null ? 401 : 200);
-
         return gson.toJson(mergeRequestDTOS);
+    }
+
+    private Date calculateUntil(String until) {
+        if(until == null)
+            return new Date(); // until now
+        else
+            return new Date(Long.parseLong(until) * epochMillisecondScale); // until given value
+    }
+
+    private Date calculateSince(String since) {
+        if(since == null)
+            return new Date(0); //since the beginning of time
+        else
+            return new Date(Long.parseLong(since) * epochMillisecondScale); // since given value
     }
 
     @GetMapping("/api/project/{projectId}/mergerequest/{mergerequestId}/commits")
