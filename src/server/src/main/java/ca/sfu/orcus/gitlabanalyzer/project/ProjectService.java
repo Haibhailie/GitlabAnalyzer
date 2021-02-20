16f402d364cb3e6839f27b735c4ace1c5fdf4954
Karan Pathania <kpathani@sfu.ca>
2021-02-20 01:13:48 -0800

    Add authentication logic to ProjectService

--- src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/project/ProjectService.java
+++ src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/project/ProjectService.java
@@ -1,5 +1,8 @@
 package ca.sfu.orcus.gitlabanalyzer.project;
 
+import ca.sfu.orcus.gitlabanalyzer.authentication.AuthenticationService;
+import ca.sfu.orcus.gitlabanalyzer.member.MemberDTO;
+import ca.sfu.orcus.gitlabanalyzer.member.MemberService;
 import org.gitlab4j.api.GitLabApi;
 import org.gitlab4j.api.GitLabApiException;
 import org.gitlab4j.api.models.Project;
@@ -12,28 +15,57 @@ import java.util.List;
 @Service
 public class ProjectService {
     private final ProjectRepository projectRepository;
+    private final AuthenticationService authService;
+    private final MemberService memberService;
 
     @Autowired
-    public ProjectService(ProjectRepository projectRepository) {
+    public ProjectService(ProjectRepository projectRepository, AuthenticationService authService, MemberService memberService) {
         this.projectRepository = projectRepository;
+        this.authService = authService;
+        this.memberService = memberService;
     }
 
-    public ArrayList<ProjectDto> getAllProjects(GitLabApi gitLabApi) throws GitLabApiException {
-        ArrayList<ProjectDto> projectDtos = new ArrayList<>();
-        List<Project> projects = gitLabApi.getProjectApi().getProjects();
-        for (Project p: projects) {
-            ProjectDto projectDto = new ProjectDto(gitLabApi, p);
-            projectDtos.add(projectDto);
+    public List<ProjectDto> getAllProjects(String jwt) {
+        GitLabApi gitLabApi = authService.getGitLabApiFor(jwt);
+        if (gitLabApi != null) {
+            return getAllProjects(gitLabApi);
+        } else {
+            return null;
         }
-
-        return projectDtos;
     }
 
-    public ProjectDto getProject(GitLabApi gitLabApi, int projectId) throws GitLabApiException {
-        Project project = gitLabApi.getProjectApi().getProject(projectId);
-        ProjectDto projectDto = new ProjectDto(gitLabApi, project);
+    public ArrayList<ProjectDto> getAllProjects(GitLabApi gitLabApi) {
+        try {
+            ArrayList<ProjectDto> projectDtos = new ArrayList<>();
+            List<Project> projects = gitLabApi.getProjectApi().getMemberProjects();
+            for (Project p: projects) {
+                ProjectDto projectDto = new ProjectDto(p);
+                projectDtos.add(projectDto);
+            }
+            return projectDtos;
+        } catch (GitLabApiException e) {
+            return null;
+        }
+    }
 
-        return projectDto;
+    public ProjectExtendedDto getProject(String jwt, int projectId) {
+        GitLabApi gitLabApi = authService.getGitLabApiFor(jwt);
+        if (gitLabApi != null) {
+            return getProject(gitLabApi, projectId);
+        } else {
+            return null;
+        }
+    }
+
+    public ProjectExtendedDto getProject(GitLabApi gitLabApi, int projectId) {
+        try {
+            Project project = gitLabApi.getProjectApi().getProject(projectId, true);
+            Long numBranches = (long) gitLabApi.getRepositoryApi().getBranches(projectId).size();
+            List<MemberDTO> memberDtos = memberService.getAllMemberDTOs(gitLabApi, projectId);
+            return new ProjectExtendedDto(project, memberDtos, numBranches);
+        } catch (GitLabApiException e) {
+            return null;
+        }
     }
 
 }
