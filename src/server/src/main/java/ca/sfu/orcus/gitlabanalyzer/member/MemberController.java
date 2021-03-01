67087f176c0c9508074e03fa9a947b848aead859
LIYANG ZHOU <liyang_zhou_2@sfu.ca>
2021-02-28 23:04:15 -0800

    test gitlab ci

--- src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/member/MemberController.java
+++ src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/member/MemberController.java
@@ -30,6 +30,8 @@ public class MemberController {
         response.setStatus(members == null ? 401 : 200);
         Gson gson = new Gson();
         return gson.toJson(members);
+
+        
     }
 
     @GetMapping("/api/project/{projectId}/members/{memberEmail}/commits")
