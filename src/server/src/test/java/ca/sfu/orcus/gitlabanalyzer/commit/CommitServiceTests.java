826dfbf29c3ce17e305113c5adb8e72fbfea45d1
Taha Liaqat <tliaqat@sfu.ca>
2021-03-01 17:17:24 -0800

    Initial commit. Add commitServiceTests.java

---
+++ src/server/src/test/java/ca/sfu/orcus/gitlabanalyzer/commit/CommitServiceTests.java
@@ -0,0 +1,11 @@
+package ca.sfu.orcus.gitlabanalyzer.commit;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class CommitServiceTests {
+    @Test
+    void commitServiceTest() {
+        System.out.println("Youre welcome");
+    }
+}
