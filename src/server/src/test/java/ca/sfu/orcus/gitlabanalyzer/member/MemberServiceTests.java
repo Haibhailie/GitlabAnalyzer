67b6762e0659e05adfb3ef9fefc63d52585ad97d
LIYANG ZHOU <liyang_zhou_2@sfu.ca>
2021-03-02 13:08:32 -0800

    member test initial

---
+++ src/server/src/test/java/ca/sfu/orcus/gitlabanalyzer/member/MemberServiceTests.java
@@ -0,0 +1,11 @@
+package ca.sfu.orcus.gitlabanalyzer.member;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class MemberServiceTests {
+    @Test
+    void memberServiceTest() {
+        System.out.println("Member test");
+    }
+}
