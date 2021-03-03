53f7c62ecd6e68d50cb51b35b8cadaa9fafb7e5d
Bryan Niwa <bryan_niwa@sfu.ca>
2021-03-02 17:21:05 -0800

    Change class declaration to final

--- src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/utils/VariableDecoderUtil.java
+++ src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/utils/VariableDecoderUtil.java
@@ -2,7 +2,7 @@ package ca.sfu.orcus.gitlabanalyzer.utils;
 
 import java.util.Base64;
 
-public class VariableDecoderUtil {
+public final class VariableDecoderUtil {
     public static String decode(String base) {
         byte[] decodedBytes = Base64.getDecoder().decode(System.getenv(base));
         return new String(decodedBytes);
