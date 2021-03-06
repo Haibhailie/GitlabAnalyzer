bef32d6ca4e7c2e92bb2cf0f1659d413af23eb8c
Grace Luo <grace_luo@sfu.ca>
2021-03-05 22:08:28 -0800

    Setup skeleton for MemberSummary and Member page

---
+++ src/client/src/components/MemberSummary.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+import StatSummary from '../components/StatSummary'
+import ActivityGraph from '../components/ActivityGraph'
+
+import styles from '../css/MemberSummary.module.css'
+
+const MemberSummary = () => {
+  return <div className={styles.container}></div>
+}
+
+export default MemberSummary
---
+++ src/client/src/pages/Member.tsx
@@ -0,0 +1,17 @@
+import jsonFetcher from '../utils/jsonFetcher'
+import useSuspense from '../utils/useSuspense'
+import { useParams } from 'react-router-dom'
+import { onError } from '../utils/suspenseDefaults'
+
+import Loading from '../components/Loading'
+import ErrorComp from '../components/ErrorComp'
+import Selector from '../components/Selector'
+import MemberSummary from '../components/MemberSummary'
+
+import styles from '../css/Member.module.css'
+
+const Member = () => {
+  return <div></div>
+}
+
+export default Member
