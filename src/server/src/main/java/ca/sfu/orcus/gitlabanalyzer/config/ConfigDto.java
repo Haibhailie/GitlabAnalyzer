9622c939bb982fa3e18b6005e30a12fca3473e48
Shashank Thanalapati <shashank_thanalapati@sfu.ca>
2021-03-07 15:29:05 -0800

    Added basic variables in ConfigDto

--- src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/config/ConfigDto.java
+++ src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/config/ConfigDto.java
@@ -1,5 +1,18 @@
 package ca.sfu.orcus.gitlabanalyzer.config;
 
+import java.util.List;
+import java.util.Map;
+
 public class ConfigDto {
+
+    private double diffAdditionMultiplier;
+    private double diffDeletionMultiplier;
+    private double diffCommentMultiplier;
+    private double diffSpacingMultiplier;
+    private double diffSyntaxChangeMultiplier;
+
+    private static final Map<Integer, String> languageScores = Map.of();
+
+
 
 }
