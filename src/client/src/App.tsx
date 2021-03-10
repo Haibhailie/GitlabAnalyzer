6c1c1e0f7b8534dfca07ffb3a376eb653eb0b623
djso <djso@sfu.ca>
2021-03-10 01:37:16 -0800

    add user config provider to app

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -12,6 +12,7 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Project from './pages/Project'
 import PageWrapper from './components/PageWrapper'
+import UserConfigProvider from './context/UserConfigContext'
 
 const AuthCheck = () => {
   const history = useHistory()
@@ -31,30 +32,32 @@ const AuthCheck = () => {
 
 const App = () => {
   return (
-    <Provider>
-      <Router>
-        <Switch>
-          <Route path="/home">
-            <PageWrapper>
-              <Home />
-            </PageWrapper>
-          </Route>
-          <Route path="/project/:id">
-            <PageWrapper>
-              <Project />
-            </PageWrapper>
-          </Route>
-        </Switch>
-        <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/">
-            <AuthCheck />
-          </Route>
-        </Switch>
-      </Router>
-    </Provider>
+    <UserConfigProvider>
+      <Provider>
+        <Router>
+          <Switch>
+            <Route path="/home">
+              <PageWrapper>
+                <Home />
+              </PageWrapper>
+            </Route>
+            <Route path="/project/:id">
+              <PageWrapper>
+                <Project />
+              </PageWrapper>
+            </Route>
+          </Switch>
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/">
+              <AuthCheck />
+            </Route>
+          </Switch>
+        </Router>
+      </Provider>
+    </UserConfigProvider>
   )
 }
 
