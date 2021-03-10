a89d4460393384c9a12dd113e44956b82f6a903f
Karan Pathania <kpathani@sfu.ca>
2021-03-10 13:35:08 -0800

    Set up JwtServiceTest outline

--- src/server/src/test/java/ca/sfu/orcus/gitlabanalyzer/authentication/JwtServiceTest.java
+++ src/server/src/test/java/ca/sfu/orcus/gitlabanalyzer/authentication/JwtServiceTest.java
@@ -1,4 +1,16 @@
 package ca.sfu.orcus.gitlabanalyzer.authentication;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class JwtServiceTest {
+    @Mock private JwtService jwtService;
+
+    // Test createJwt()
+
+    // Test jwtIsValid()
+
+    // Test getType()
 }
