9ecd0f1212760e9bb98b7ab2dc833462042d0fd0
Karan Pathania <kpathani@sfu.ca>
2021-03-11 01:25:24 -0800

    Update test setup

--- src/server/src/test/java/ca/sfu/orcus/gitlabanalyzer/authentication/JwtServiceTest.java
+++ src/server/src/test/java/ca/sfu/orcus/gitlabanalyzer/authentication/JwtServiceTest.java
@@ -1,16 +1,50 @@
 package ca.sfu.orcus.gitlabanalyzer.authentication;
 
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import ca.sfu.orcus.gitlabanalyzer.utils.VariableDecoderUtil;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.io.Encoders;
+import io.jsonwebtoken.security.Keys;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.MockedStatic;
 
-@ExtendWith(MockitoExtension.class)
+import javax.crypto.SecretKey;
+
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.mockStatic;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class JwtServiceTest {
-    @Mock private JwtService jwtService;
+    private JwtService jwtService;
+    private SecretKey secretKey;
 
-    // Test createJwt()
+    @BeforeAll
+    public void setup() {
+        String encodedSecretKey = generateBase64EncodedSecretKey();
+        jwtService = createJwtServiceWithSecretKey(encodedSecretKey);
+        secretKey = getSecretKeyFromBase64EncodedString(encodedSecretKey);
+    }
 
-    // Test jwtIsValid()
+    private JwtService createJwtServiceWithSecretKey(String encodedSecretKey) {
+        try (MockedStatic<VariableDecoderUtil> mockedVariableDecoderUtil = mockStatic(VariableDecoderUtil.class)) {
+            mockedVariableDecoderUtil.when(() -> {
+                VariableDecoderUtil.decode(anyString());
+            }).thenReturn(encodedSecretKey);
 
-    // Test getType()
+            return new JwtService();
+        }
+    }
+
+    private String generateBase64EncodedSecretKey() {
+        SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+        return Encoders.BASE64.encode(secretKey.getEncoded());
+    }
+
+    private SecretKey getSecretKeyFromBase64EncodedString(String encodedKey) {
+        byte[] decodedKey = Decoders.BASE64.decode(encodedKey);
+        return Keys.hmacShaKeyFor(decodedKey);
+    }
+
+
 }
