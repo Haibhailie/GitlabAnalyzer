99d4e6b21bb4f8905303a1d8d6f1eb7f27aa4648
Bryan Niwa <bryan_niwa@sfu.ca>
2021-03-12 15:35:45 -0800

    Fix GitLabApiWrapper tests to not depend on our server
    Use mockito's constructor mock feature to avoid accessing the actual gitlab server

--- src/server/src/test/java/ca/sfu/orcus/gitlabanalyzer/authentication/GitLabApiWrapperTest.java
+++ src/server/src/test/java/ca/sfu/orcus/gitlabanalyzer/authentication/GitLabApiWrapperTest.java
@@ -1,18 +1,24 @@
 package ca.sfu.orcus.gitlabanalyzer.authentication;
 
+import org.gitlab4j.api.GitLabApi;
+import org.gitlab4j.api.GitLabApiException;
+import org.gitlab4j.api.UserApi;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedConstruction;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.mockConstruction;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class GitLabApiWrapperTest {
     @Mock JwtService jwtService;
     @Mock AuthenticationRepository authRepository;
+    @Mock UserApi userApi;
 
     @InjectMocks
     GitLabApiWrapper gitLabApiWrapper;
@@ -26,20 +32,30 @@ class GitLabApiWrapperTest {
     }
 
     @Test
-    public void failGetGitLabApiForInvalidPat() {
+    public void failGetGitLabApiForInvalidPat() throws GitLabApiException {
         when(jwtService.jwtIsValid(sampleJwt)).thenReturn(true);
         when(jwtService.getType(sampleJwt)).thenReturn(JwtService.JwtType.PAT);
         String samplePat = "samplePat";
         when(authRepository.getPatFor(sampleJwt)).thenReturn(samplePat);
-        assertNull(gitLabApiWrapper.getGitLabApiFor(sampleJwt));
+
+        assertNull(getGitLabApiWithGetUserApiMocked());
     }
 
     @Test
-    public void failGetGitLabApiForInvalidUserPass() {
+    public void failGetGitLabApiForInvalidUserPass() throws GitLabApiException {
         when(jwtService.jwtIsValid(sampleJwt)).thenReturn(true);
         when(jwtService.getType(sampleJwt)).thenReturn(JwtService.JwtType.USER_PASS);
         String sampleAuthToken = "sampleAuthToken";
         when(authRepository.getAuthTokenFor(sampleJwt)).thenReturn(sampleAuthToken);
-        assertNull(gitLabApiWrapper.getGitLabApiFor(sampleJwt));
+
+        assertNull(getGitLabApiWithGetUserApiMocked());
+    }
+
+    private GitLabApi getGitLabApiWithGetUserApiMocked() throws GitLabApiException {
+        try (MockedConstruction<GitLabApi> ignore = mockConstruction(GitLabApi.class,
+                (mock, context) -> when(mock.getUserApi()).thenReturn(userApi))) {
+            when(userApi.getCurrentUser()).thenThrow(GitLabApiException.class);
+            return gitLabApiWrapper.getGitLabApiFor(sampleJwt);
+        }
     }
 }
