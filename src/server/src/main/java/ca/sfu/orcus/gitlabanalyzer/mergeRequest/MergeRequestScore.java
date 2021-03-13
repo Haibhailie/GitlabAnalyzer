93e47c27fc98762c239162ac953eb7980b46c757
Shashank Thanalapati <shashank_thanalapati@sfu.ca>
2021-03-13 02:55:58 -0800

    Check Syntax change working (making sure it doesn't also add syntax changes + additions)

--- src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/mergeRequest/MergeRequestScore.java
+++ src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/mergeRequest/MergeRequestScore.java
@@ -1,12 +1,13 @@
 package ca.sfu.orcus.gitlabanalyzer.mergeRequest;
 
 import ca.sfu.orcus.gitlabanalyzer.utils.DiffStringParser;
+import org.apache.commons.lang3.StringUtils;
 import org.gitlab4j.api.GitLabApi;
 import org.gitlab4j.api.GitLabApiException;
 import org.gitlab4j.api.models.MergeRequest;
 
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MergeRequestScore {
@@ -28,26 +29,31 @@ public class MergeRequestScore {
     public double getMergeRequestScore(GitLabApi gitLabApi, int projectId, int mergeRequestId) throws GitLabApiException {
         double totalScore = 0;
         MergeRequest mergeRequestChanges = gitLabApi.getMergeRequestApi().getMergeRequestChanges(projectId, mergeRequestId);
-        String[] diffString = DiffStringParser.parseDiff(mergeRequestChanges.getChanges()).split("\\r?\\n");
-        parseDiffList(diffString);
+
+        //regex to split lines by new line and store in generatedDiffList
+        generatedDiffList = Arrays.asList(DiffStringParser.parseDiff(mergeRequestChanges.getChanges()).split("\\r?\\n"));
+
+        parseDiffList();
+
         totalScore += (numLineAdditions * addLOCFactor) +
                 (numLineDeletions * deleteLOCFactor) +
                 (numBlankAdditions * blankLOCFactor) +
                 (numSyntaxChanges * syntaxChangeFactor) +
                 (numSpacingChanges * spacingChangeFactor);
+
         System.out.println(totalScore);
         return totalScore;
     }
 
-    private void parseDiffList(String[] diffsList) {
-        for (String line : diffsList) {
+    private void parseDiffList() {
+        int lineNumber = -1;
+        for (String line : generatedDiffList) {
+            lineNumber++;
             if (line.startsWith("---")) {
-                continue;
-            }
-            else if (line.startsWith("+++")) {
-                continue;
-            }
-            else if (line.startsWith("+")) {
+                System.out.println("Skipped line: "+lineNumber);
+            } else if (line.startsWith("+++")) {
+                System.out.println("Skipped line "+lineNumber);
+            } else if (line.startsWith("+")) {
 
                 if (line.substring(1).length() > 0) {
                     numLineAdditions++;
@@ -56,8 +62,29 @@ public class MergeRequestScore {
                 }
             } else if (line.startsWith("-")) {
                 numLineDeletions++;
-
+                checkSyntaxChanges(lineNumber, line);
             }
         }
     }
+
+    private void checkSyntaxChanges(int lineNumber, String testingLine) {
+        int presentLine = -1;
+        for (String line : generatedDiffList) {
+            presentLine++;
+            if (presentLine < lineNumber) {
+                continue;
+            }
+            if (line.startsWith("-")) {
+                continue;
+            } else {
+                if (StringUtils.difference(testingLine, line).length() > (testingLine.length()) / 2) {
+                    numSyntaxChanges++;
+                    generatedDiffList.set(presentLine, "+++");
+                    break;
+                }
+            }
+        }
+
+    }
+
 }
