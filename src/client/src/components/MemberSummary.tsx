5414c1888b73524a818cd602cd4b7bc547ee7ee5
Grace Luo <grace_luo@sfu.ca>
2021-03-13 20:33:52 -0800

    Display member summary and stats

--- src/client/src/components/MemberSummary.tsx
+++ src/client/src/components/MemberSummary.tsx
@@ -1,5 +1,8 @@
-import { useState } from 'react'
+import jsonFetcher from '../utils/jsonFetcher'
+import useSuspense from '../utils/useSuspense'
+import { onError } from '../utils/suspenseDefaults'
 import { IMemberData, IMemberStatData } from '../pages/Member'
+import { ICommitData, IMergeData } from '../components/ActivityGraph'
 
 import StatSummary from '../components/StatSummary'
 import ActivityGraph from '../components/ActivityGraph'
@@ -9,50 +12,115 @@ import styles from '../css/MemberSummary.module.css'
 export interface IMemberSummaryProps {
   projectId: string
   memberData: IMemberData | undefined
-  memberStats: IMemberStatData | undefined
 }
 
-const MemberSummary = ({
-  projectId,
-  memberData,
-  memberStats,
-}: IMemberSummaryProps) => {
-  if (!memberData || !projectId || !memberStats) return null
+const computeStats = (
+  commitData: ICommitData[],
+  mergeData: IMergeData[]
+): IMemberStatData => {
+  return {
+    commits: commitData,
+    mergeRequests: mergeData,
+  } as IMemberStatData
+}
 
-  const { id, username, displayName, role } = memberData
-  const {
-    numCommits,
-    commitScore,
-    numMergeRequests,
-    mergeRequestScore,
-  } = memberStats
+const computeCommitScore = (commitData: ICommitData[] | undefined): number => {
+  if (!commitData) {
+    return 0
+  }
+  let commitScore = 0
+  for (const commit of commitData) {
+    const score = Math.floor(Math.random() * 100 + commit.author.length)
+    commitScore += score
+  }
+  return commitScore
+}
 
-  console.log(displayName)
+const computeMergeScore = (
+  mergeRequestData: IMergeData[] | undefined
+): number => {
+  if (!mergeRequestData) {
+    return 0
+  }
+  let mergeScore = 0
+  for (const mergeRequest of mergeRequestData) {
+    const score = Math.floor(Math.random() * 50 + mergeRequest.author.length)
+    mergeScore += score
+  }
+  return mergeScore
+}
+
+const computeLinesAdded = (commitData: ICommitData[] | undefined): number => {
+  if (!commitData) {
+    return 0
+  }
+  let totalLineAdditions = 0
+  for (const commit of commitData) {
+    const lines = Math.floor(Math.random() * 100 + commit.author.length)
+    totalLineAdditions += lines
+  }
+  return totalLineAdditions
+}
+
+const MemberSummary = ({ projectId, memberData }: IMemberSummaryProps) => {
+  if (!memberData || !projectId) return null
+
+  const { id, displayName } = memberData
+
+  const { Suspense: DataSuspense, data, error } = useSuspense<
+    IMemberStatData,
+    Error
+  >((setData, setError) => {
+    let otherData: ICommitData[] | IMergeData[] | null = null
+    jsonFetcher<IMergeData[]>(
+      `/api/project/${projectId}/members/${id}/mergerequests`
+    )
+      .then(data => {
+        if (otherData) {
+          setData(computeStats(otherData as ICommitData[], data))
+        } else {
+          otherData = data
+        }
+      })
+      .catch(onError(setError))
+    jsonFetcher<ICommitData[]>(
+      `/api/project/${projectId}/members/${displayName}/commits`
+    )
+      .then(data => {
+        if (otherData) {
+          setData(computeStats(data, otherData as IMergeData[]))
+        } else {
+          otherData = data
+        }
+      })
+      .catch(onError(setError))
+  })
 
   const memberStatData = [
     {
       name: 'Merge request score',
-      value: mergeRequestScore,
+      value: computeMergeScore(data?.mergeRequests),
+      description: 'Sum of merge request scores for selected date range',
     },
     {
       name: 'Commit score',
-      value: commitScore,
+      value: computeCommitScore(data?.commits),
       description: 'Sum of commit scores for selected date range',
     },
     {
       name: 'Total merge requests',
-      value: numMergeRequests,
+      value: data?.mergeRequests.length,
       description: 'Number of merge requests made',
     },
 
     {
       name: 'Total commits',
-      value: numCommits,
+      value: data?.commits.length,
       description: 'Number of commits made',
     },
     {
-      name: 'Lines of code',
-      value: 399,
+      name: 'Lines of code added',
+      value: computeLinesAdded(data?.commits),
     },
   ]
 
@@ -60,11 +128,16 @@ const MemberSummary = ({
     <div className={styles.container}>
       <div className={styles.statsContainer}>
         <div className={styles.graph}>
-          <ActivityGraph
-            mergeUrl={`/api/project/${projectId}/members/${id}/mergerequests`}
-            commitUrl={`api/project/${projectId}/members/${displayName}/commits`}
-            yAxisValue={'number'}
-          />
+          <DataSuspense
+            fallback={`Analyzing ${memberData?.displayName} . . . `}
+            error={error?.message ?? 'Unknown Error'}
+          >
+            <ActivityGraph
+              mergeUrl={`/api/project/${projectId}/members/${id}/mergerequests`}
+              commitUrl={`/api/project/${projectId}/members/${displayName}/commits`}
+              yAxisValue={'number'}
+            />
+          </DataSuspense>
         </div>
         <StatSummary statData={memberStatData} />
       </div>
--- src/client/src/pages/Member.tsx
+++ src/client/src/pages/Member.tsx
@@ -2,12 +2,7 @@ import { useLocation, useParams } from 'react-router-dom'
 import jsonFetcher from '../utils/jsonFetcher'
 import useSuspense from '../utils/useSuspense'
 import { onError } from '../utils/suspenseDefaults'
-
-import {
-  ICommitData,
-  IMergeData,
-  IActivityData,
-} from '../components/ActivityGraph'
+import { ICommitData, IMergeData } from '../components/ActivityGraph'
 
 import Selector from '../components/Selector'
 import MemberSummary from '../components/MemberSummary'
@@ -15,29 +10,15 @@ import MemberSummary from '../components/MemberSummary'
 import styles from '../css/Member.module.css'
 
 export interface IMemberData {
-  id: string | number
+  id: string
   username: string
   displayName: string
   role: string
 }
 
 export interface IMemberStatData {
-  numCommits: number | undefined
-  commitScore: number
-  numMergeRequests: number
-  mergeRequestScore: number
-}
-
-const computeStats = (
-  commitData: ICommitData[],
-  mergeData: IMergeData[]
-): IMemberStatData => {
-  return {
-    numCommits: commitData.length,
-    numMergeRequests: mergeData.length,
-    commitScore: 242,
-    mergeRequestScore: 166,
-  } as IMemberStatData
+  commits: ICommitData[]
+  mergeRequests: IMergeData[]
 }
 
 const Member = () => {
@@ -64,37 +45,6 @@ const Member = () => {
     }
   })
 
-  const { Suspense: DataSuspense, data, error } = useSuspense<
-    IMemberStatData,
-    Error
-  >((setData, setError) => {
-    let otherData: ICommitData[] | IMergeData[] | null = null
-    jsonFetcher<IMergeData[]>(
-      `/api/project/${id}/members/${memberId}/mergerequests`
-    )
-      .then(data => {
-        console.log(data)
-        if (otherData) {
-          setData(computeStats(otherData as ICommitData[], data))
-        } else {
-          otherData = data
-        }
-      })
-      .catch(onError(setError))
-    jsonFetcher<ICommitData[]>(
-      `/api/project/${id}/members/${memberData?.displayName}/commits`
-    )
-      .then(data => {
-        console.log(data)
-        if (otherData) {
-          setData(computeStats(data, otherData as IMergeData[]))
-        } else {
-          otherData = data
-        }
-      })
-      .catch(onError(setError))
-  })
-
   return (
     <MemberSuspense
       fallback="Getting member details..."
@@ -102,19 +52,13 @@ const Member = () => {
     >
       <div className={styles.container}>
         <h1 className={styles.header}>{memberData?.displayName}</h1>
+        <h3 className={styles.subheader}>
+          {memberData?.username && `@${memberData?.username}`}
+        </h3>
         <Selector tabHeaders={['Summary', 'Merge Requests', 'Comments']}>
-          <DataSuspense
-            fallback={`Analyzing ${memberData?.displayName} . . `}
-            error={error?.message ?? 'Unknown Error'}
-          >
-            <div className={styles.summaryContainer}>
-              <MemberSummary
-                projectId={id}
-                memberData={memberData}
-                memberStats={data}
-              />
-            </div>
-          </DataSuspense>
+          <div className={styles.summaryContainer}>
+            <MemberSummary projectId={id} memberData={memberData} />
+          </div>
           <div className={styles.mergeRequestsContainer}>
             <h1>Merge requests</h1>
           </div>
