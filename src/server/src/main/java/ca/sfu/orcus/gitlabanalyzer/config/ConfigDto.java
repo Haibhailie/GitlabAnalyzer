b5c0e2729cfb86d73ab5fa271b1d11c498bb4768
Karan Pathania <kpathani@sfu.ca>
2021-03-14 19:01:27 -0700

    Remove unneeded setters and constructors

--- src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/config/ConfigDto.java
+++ src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/config/ConfigDto.java
@@ -1,112 +1,42 @@
 package ca.sfu.orcus.gitlabanalyzer.config;
 
 import com.google.gson.annotations.SerializedName;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 final class ConfigDto {
     private String id;
     private String name;
     private long startDate;
     private long endDate;
     private String scoreBy;
-
-    @SerializedName(value = "yAxis")
-    private String verticalAxis;
-
     private String graphMode;
     private List<GeneralTypeScoreDto> generalScores;
     private List<FileTypeScoreDto> fileScores;
+    @SerializedName(value = "yAxis")
+    private String verticalAxis;
 
-    public ConfigDto(String id,
-                     String name,
-                     long startDate,
-                     long endDate,
-                     String scoreBy,
-                     String verticalAxis,
-                     String graphMode,
-                     List<GeneralTypeScoreDto> generalScores,
-                     List<FileTypeScoreDto> fileScores) {
-        setId(id);
-        setName(name);
-        setStartDate(startDate);
-        setEndDate(endDate);
-        setScoreBy(scoreBy);
-        setVerticalAxis(verticalAxis);
-        setGraphMode(graphMode);
-        setGeneralScores(generalScores);
-        setFileScores(fileScores);
-    }
+    private ConfigDto() {}
 
     public void setId(String id) {
         this.id = id;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setStartDate(long startDate) {
-        this.startDate = startDate;
-    }
-
-    public void setEndDate(long endDate) {
-        this.endDate = endDate;
-    }
-
-    public void setScoreBy(String scoreBy) {
-        this.scoreBy = scoreBy;
-    }
-
-    public void setVerticalAxis(String verticalAxis) {
-        this.verticalAxis = verticalAxis;
-    }
-
-    public void setGraphMode(String graphMode) {
-        this.graphMode = graphMode;
-    }
-
-    public void setGeneralScores(List<GeneralTypeScoreDto> generalScores) {
-        this.generalScores = generalScores;
-    }
-
-    public void setFileScores(List<FileTypeScoreDto> fileScores) {
-        this.fileScores = fileScores;
-    }
+    // Nested class types
 
     private static final class FileTypeScoreDto {
         private String fileExtension;
         private int scoreMultiplier;
 
-        FileTypeScoreDto(String fileExtension, int scoreMultiplier) {
-            setFileExtension(fileExtension);
-            setScoreMultiplier(scoreMultiplier);
-        }
-
-        public void setFileExtension(String fileExtension) {
-            this.fileExtension = fileExtension;
-        }
-
-        public void setScoreMultiplier(int scoreMultiplier) {
-            this.scoreMultiplier = scoreMultiplier;
-        }
+        private FileTypeScoreDto() {}
     }
 
     private static final class GeneralTypeScoreDto {
         private String type;
         private int value;
 
-        public GeneralTypeScoreDto(String type, int value) {
-            setType(type);
-            setValue(value);
-        }
-
-        public void setType(String type) {
-            this.type = type;
-        }
-
-        public void setValue(int value) {
-            this.value = value;
-        }
+        private GeneralTypeScoreDto() {}
     }
 }
