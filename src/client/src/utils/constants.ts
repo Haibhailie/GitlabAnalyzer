df45f2a678dc542c02fa6efa565dfb5b5cc0cb21
Grace Luo <grace_luo@sfu.ca>
2021-03-15 00:55:21 -0700

    Display actual commit scores

--- src/client/src/utils/constants.ts
+++ src/client/src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const URLBASE = 'http://localhost:8081'
+export const URLBASE = ''
 
 export const NETWORK_ERROR = 'Could not connect to server.'
 export const SERVER_ERROR = 'Server error. Please try again.'
