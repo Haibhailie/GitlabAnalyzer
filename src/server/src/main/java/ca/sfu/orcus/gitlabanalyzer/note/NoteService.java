7370695da47417fbfa30fe46c27cf9f9549088d5
LIYANG ZHOU <liyang_zhou_2@sfu.ca>
2021-03-20 23:32:35 -0700

    add NoteService.java

--- src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/note/NoteService.java
+++ src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/note/NoteService.java
@@ -1,4 +1,55 @@
-package ca.sfu.orcus.gitlabanalyzer.comment;
+package ca.sfu.orcus.gitlabanalyzer.note;
 
-public class CommentService {
+import ca.sfu.orcus.gitlabanalyzer.authentication.GitLabApiWrapper;
+import org.gitlab4j.api.GitLabApi;
+import org.gitlab4j.api.GitLabApiException;
+import org.gitlab4j.api.models.Issue;
+import org.gitlab4j.api.models.MergeRequest;
+import org.gitlab4j.api.models.Note;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class NoteService {
+    private final NoteRepository noteRepository;
+    private final GitLabApiWrapper gitLabApiWrapper;
+
+    @Autowired
+    public NoteService(NoteRepository noteRepository, GitLabApiWrapper gitLabApiWrapper) {
+        this.noteRepository = noteRepository;
+        this.gitLabApiWrapper = gitLabApiWrapper;
+    }
+
+
+
+    public List<NoteDto> getNotesByMemberId(String jwt, int projectId, int memberId) {
+        GitLabApi gitLabApi = gitLabApiWrapper.getGitLabApiFor(jwt);
+        if (gitLabApi == null) {
+            return null;
+        }
+        try {
+            List<MergeRequest> allMergeRequests = gitLabApi.getMergeRequestApi().getMergeRequests(projectId);
+            List<Issue> allIssues = (List<Issue>) gitLabApi.getIssuesApi().getIssues(projectId);
+            List<NoteDto> filteredNotes = new ArrayList<>();
+            List<Note> allNotes = new ArrayList<>();
+            for (MergeRequest mr : allMergeRequests) {
+                allNotes.addAll(gitLabApi.getNotesApi().getMergeRequestNotes(projectId, mr.getIid()));
+            }
+            for (Issue issue : allIssues) {
+                allNotes.addAll(gitLabApi.getNotesApi().getIssueNotes(projectId, issue.getIid()));
+            }
+            for (Note n: allNotes) {
+                if(n.getAuthor().getId() == memberId) {
+                    filteredNotes.add(new NoteDto(n));
+                }
+            }
+            return filteredNotes;
+        } catch (GitLabApiException e) {
+            return null;
+        }
+    }
+
 }
