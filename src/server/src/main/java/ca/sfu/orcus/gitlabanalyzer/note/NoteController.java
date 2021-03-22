072e252d2e0cf10d981db9d4b4cd4df47769c020
LIYANG ZHOU <liyang_zhou_2@sfu.ca>
2021-03-22 00:40:40 -0700

    add NoteController

--- src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/note/NoteController.java
+++ src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/note/NoteController.java
@@ -1,4 +1,32 @@
 package ca.sfu.orcus.gitlabanalyzer.note;
 
+import ca.sfu.orcus.gitlabanalyzer.Constants;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class NoteController {
+    private final NoteService noteService;
+
+    @Autowired
+    public NoteController(NoteService noteService) {
+        this.noteService = noteService;
+    }
+
+    @GetMapping("/api/project/{projectId}/members/{memberId}/notes")
+    public String getCommitsByMemberName(@CookieValue(value = "sessionId") String jwt,
+                                         HttpServletResponse response,
+                                         @PathVariable int projectId,
+                                         @PathVariable int memberId) {
+        List<NoteDto> allNotesByMemberId = noteService.getNotesDtosByMemberId(jwt, projectId, memberId);
+        response.setStatus(allNotesByMemberId == null ? 401 : 200);
+        Gson gson = new Gson();
+        return gson.toJson(allNotesByMemberId);
+    }
+
 }
