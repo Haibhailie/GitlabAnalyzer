bf13d7a566929dac3e477ead30d3a4f3ca785aa4
Shashank Thanalapati <shashank_thanalapati@sfu.ca>
2021-04-08 14:00:51 -0700

    Created a base MRRepository class

--- src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/mergeRequest/MergeRequestRepository.java
+++ src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/mergeRequest/MergeRequestRepository.java
@@ -1,12 +1,43 @@
 package ca.sfu.orcus.gitlabanalyzer.mergeRequest;
 
+import ca.sfu.orcus.gitlabanalyzer.utils.VariableDecoderUtil;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class MergeRequestRepository {
-    public MergeRequestRepository() {
 
+    private final MongoCollection<Document> mergeRequestCollection;
+    private enum MergeRequest {
+        documentId("mergeRequestId"),
+        title("title"),
+        author("author"),
+        description("description"),
+        time("time"),
+        webUrl("webUrl"),
+        sumOfCommitsScore("sumOfCommitsScore"),
+        commits("commits"),
+        committers("committers"),
+        files("files"),
+        isIgnored("isIgnored");
+
+        public String key;
+
+        MergeRequest(String key) {
+            this.key = key;
+        }
     }
-}
 
+    public MergeRequestRepository() {
+        MongoClient mongoClient = MongoClients.create(VariableDecoderUtil.decode("MONGO_URI"));
+        MongoDatabase database = mongoClient.getDatabase(VariableDecoderUtil.decode("DATABASE"));
+        mergeRequestCollection = database.getCollection(VariableDecoderUtil.decode("MERGE_REQUEST_COLLECTION"));
+    }
+
+
+}
 
