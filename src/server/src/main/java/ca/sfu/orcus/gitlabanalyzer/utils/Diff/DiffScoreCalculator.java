414f9862b6d3930e255dde57663393616987fd85
Shashank Thanalapati <shashank_thanalapati@sfu.ca>
2021-04-10 16:54:17 -0700

    Included syntax and spacing additions and deletions in total number of additions and deletions

--- src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/utils/Diff/DiffScoreCalculator.java
+++ src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/utils/Diff/DiffScoreCalculator.java
@@ -43,9 +43,13 @@ public class DiffScoreCalculator {
                 }
             } else if (line.startsWith("-")) {
                 if (checkSpacingChanges(lineNumber, line)) {
+                    numLineAdditions++;
+                    numLineDeletions++;
                     continue;
                     //Log spacing changed line
                 } else if (checkSyntaxChanges(lineNumber, line)) {
+                    numLineAdditions++;
+                    numLineDeletions++;
                     continue;
                     //Log syntax changed line
                 } else {
