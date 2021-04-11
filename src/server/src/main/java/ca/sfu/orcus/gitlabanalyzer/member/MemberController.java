77d20dcbc1c8dcf3b933677ca0e3f91c322e725e
Bryan Niwa <bryan_niwa@sfu.ca>
2021-04-11 14:24:48 -0700

    Change member controller to use new service class

--- src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/member/MemberController.java
+++ src/server/src/main/java/ca/sfu/orcus/gitlabanalyzer/member/MemberController.java
@@ -1,5 +1,6 @@
 package ca.sfu.orcus.gitlabanalyzer.member;
 
+import ca.sfu.orcus.gitlabanalyzer.analysis.cachedDtos.MemberDtoDb;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -12,11 +13,11 @@ import static javax.servlet.http.HttpServletResponse.*;
 @RestController
 @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class MemberController {
-    private final MemberServiceDirect memberService;
+    private final MemberServiceCached memberService;
     private static final Gson gson = new Gson();
 
     @Autowired
-    public MemberController(MemberServiceDirect memberService) {
+    public MemberController(MemberServiceCached memberService) {
         this.memberService = memberService;
     }
 
@@ -24,12 +25,12 @@ public class MemberController {
     public String getMembers(@CookieValue(value = "sessionId") String jwt,
                              HttpServletResponse response,
                              @PathVariable int projectId) {
-        List<MemberDto> members = memberService.getAllMembers(jwt, projectId);
+        List<MemberDtoDb> members = memberService.getAllMembers(jwt, projectId);
         response.setStatus(getResponseCode(members));
         return gson.toJson(members);
     }
 
-    private int getResponseCode(List<MemberDto> list) {
+    private int getResponseCode(List<MemberDtoDb> list) {
         if (list == null) {
             return SC_UNAUTHORIZED;
         } else if (list.isEmpty()) {
